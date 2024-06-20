cb6d5b7ee5020cf4f47df0c2aedfb15ea0249373
Oliver Coates <oco36@uclive.ac.nz>
2024-06-20 20:45:12 +1200

    Ship engine control from Dev screen complete

--- Assets/Java Sea Campaign/Realtime Battle/Ship/ShipInstance.cs
+++ Assets/Java Sea Campaign/Realtime Battle/Ship/ShipInstance.cs
@@ -9,16 +9,41 @@ public class ShipInstance : MonoBehaviour
     [Header("Input:")]
     // The current state that this ships rudder is set to,
     // -1 being full port, 1 being full starboard
-    [Range(-1,1)] public float rudder;
+    [SerializeField] [Range(-1,1)] private float _rudder;
+    public float rudder
+    {
+        get
+        {
+            return _rudder;
+        }
+
+        set
+        {
+            _rudder = value;
+            OnShipStatChanged();
+        }
+    }
 
     // The target speed, that the ship's engine is accelerating towards
     // 0.0 = stop
-    // 0.2 = slow ahead
-    // 0.4 = half ahead
-    // 0.6 = cruise ahead
-    // 0.8 = full ahead
-    // 1.0 = overtime ahead
-    [Range(0,1)] public float targetSpeed;
+    // 0.25 = slow ahead
+    // 0.5 = half ahead
+    // 0.75 = cruise ahead
+    // 1.0 = full ahead
+    [SerializeField] [Range(0,1)] private float _targetSpeed;
+    public float targetSpeed
+    {
+        get
+        {
+            return _targetSpeed;
+        }
+
+        set
+        {
+            _targetSpeed = value;
+            OnShipStatChanged();
+        }
+    }
 
 
     [Header("Components:")]
@@ -36,11 +61,6 @@ public class ShipInstance : MonoBehaviour
         if (GameMaster.turnUnderway)
         {
             _engine.TurnUpdate(Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.R))
-        {
-            OnShipStatChanged();
         }
     }
 
