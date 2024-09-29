8ebd8f307e5350f1e428ec232df1e318e502b527
Oliver Coates <oco36@uclive.ac.nz>
2024-09-30 12:15:22 +1300

    Cinematic State object set up with some basic sprites

---
+++ Assets/Java Asunder/Game/Cinematic State/CinematicState.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Cinematic State", menuName = "Java Asunder/Cinematics/CinematicState", order = 1)]
+public class CinematicState : ScriptableObject
+{
+    public Sprite[] displaySprites;
+}
---
+++ Assets/Java Asunder/Game/Cinematic State/CinematicStateManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicStateManager : MonoBehaviour
+{
+    [SerializeField] private CinematicState[] _states;
+    private CinematicState _currentState;
+
+    public void TransitionToState()
+    {
+        
+    }
+}
